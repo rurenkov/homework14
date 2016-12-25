c92177a5e207599ff3d91353c639c92db6e77c7a
Vitalii Rurenko <rurenkov@gmail.com>
2016-12-26 01:11:53 +0200

    first commit

---
+++ WinForm/BasedOnArray/Buffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace BasedOnArray
+{
+    public abstract class Buffer<T> : IBuffer<T>
+    {
+
+        public T[] Array;
+        public int head;
+        public int free;
+        public T put;
+
+        public Buffer(T[] array)
+        {
+            Array = array;
+
+            free = Array.Length - head;
+        }
+
+
+
+        public abstract bool IsFull();
+        public abstract bool IsEmpty();
+        public abstract void Print();
+
+
+    }
+}
---
+++ WinForm/Common/IMyStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IMyStack<T> : IBuffer<T>
+    {
+        void Push(T put);
+        T Pop();
+        T Peek();
+    }
+}
